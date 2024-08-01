ef03a4f2465c4ff75c787e95c2720af2abfaf3d5
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2024-08-01 14:24:16 +0400

    improvment of the function

--- fun/compareImage2Pattern.m
+++ fun/compareImage2Pattern.m
@@ -1,15 +1,33 @@
-function [errMatrix] = compareImage2Pattern(Img,patternImage)
+function [errMatrix] = compareImage2Pattern(Img,patternImage,method)
+
+if nargin < 3
+    method = "Include"; % default method
+end
 
 lengthBox = size(patternImage,1);
-errMatrix = ones(size(Img,1),size(Img,2));
+errMatrix = nan(size(Img,2),size(Img,1));
 
-for row = 1 : size(Img,1)
-    for col = 1 : size(Img,2)
+if method == "Include"
+    lengthBorder = 0;
+elseif method == "Crop"
+    lengthBorder = floor(lengthBox/2);
+else 
+    error('method enable: %s', method);
+end
+
+
+
+
+
+for row = 1+lengthBorder : size(Img,2)
+    for col = 1+lengthBorder : size(Img,1)
         temp = extractPattern(Img,row,col,lengthBox);
         if ~isnan(temp)
             errMatrix(row,col) =  comparaisonImage(temp,patternImage);
         end
-
     end
 end
+
+errMatrix = errMatrix'; % transpose for representation 
+
 end
