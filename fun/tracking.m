23eec53266c33200a085fd8aa32119085bcd6542
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2024-08-06 13:55:03 +0400

    tracking function (template matching)

---
+++ fun/tracking.m
@@ -0,0 +1,62 @@
+function [row,col,minValue] = tracking(frames,template,currentTemplateRow,currentTemplateCol,nPixRow,nPixCol,rateOfChange)
+compt = 1;
+nbFrames = numel(frames);
+method = "Crop";
+    % size of the zone of interest
+nTemplateRow = size(template,1);
+nTemplateCol = size(template,2);
+heigthBox = 2*nPixRow + nTemplateRow;
+widthBox = 2*nPixCol + nTemplateCol;
+
+if mod(length(heigthBox),2)~=1 && mod(length(widthBox),2)~=1
+    error('error in function tracking')
+end
+
+    % intitialize 
+row = [currentTemplateRow,nan(1,nbFrames-1)];
+col = [currentTemplateCol,nan(1,nbFrames-1)];
+minValue = [0,nan(1,nbFrames-1)];
+
+figure
+for currentFrame = 2 : nbFrames
+    currentImage = frames{currentFrame};
+    currentZoneOfInterest = corpImageAsRectangle(currentImage, ...         % curent image to analyse 
+        currentTemplateCol, ...                                           % previous position 
+        currentTemplateRow, ...                                           % previous position 
+        heigthBox,widthBox);                                               % zone of intest size
+
+    errMatrix = compareImage2Pattern(currentZoneOfInterest,template,method);        % pattern matching 
+    [currentError,rowOffset,colOffset] = minError(errMatrix);    
+    
+    
+    if ~isnan(currentError)
+        minValue(currentFrame) = currentError;
+        col(currentFrame) = currentTemplateCol + colOffset;
+        row(currentFrame) = currentTemplateRow + rowOffset;
+    else
+        minValue(currentFrame) = 1;
+        col(currentFrame) = col(currentFrame-1);
+        row(currentFrame) = row(currentFrame-1);
+    end
+
+
+
+    if compt == rateOfChange
+       compt = 0;
+       currentTemplateRow = row(currentFrame);
+       currentTemplateCol = col(currentFrame);
+       
+       template = corpImageAsRectangle(currentImage, ...
+           currentTemplateCol, ...
+           currentTemplateRow, ...
+           nTemplateRow, ...
+           nTemplateCol);
+    
+       plotErrorMartixAndImage(errMatrix,currentZoneOfInterest)
+       pause(.1)
+
+    end
+    compt = compt + 1;
+    
+end
+end
