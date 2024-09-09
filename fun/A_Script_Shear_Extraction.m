9835af1e17775e04cc5aebc70309645b2e4f9296
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2024-09-09 15:22:14 +0400

    Exemple_Shear_Extraction

--- fun/A_Script_Shear_Extraction.m
+++ fun/A_Script_Shear_Extraction.m
@@ -1,21 +1,46 @@
-% Script test
+                        %% Script test
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% get the the video and scale it
 video =  uiGetVideo(); 
 
-% Shape = setSquareShape(video.frame{1}); 
+%% video selected 
+videoIndex = 1; % first video import
+frameIndex = 1; % first frame to set the shape 
 
-Shape = setCustumShape(video.frame{1}); 
+%% read the video
+videoRead(video(videoIndex).frame,video(videoIndex).videoObject)           % read the video 
+frameRead(video(videoIndex).frame,frameIndex)                              % show a sigle frame
 
+%% show colorscale 
+ShowColorScale(video(videoIndex).colorScale.colorScaleRGBuint8, ...
+    video(videoIndex).colorScale.colorScaleValue)                          % show the color scale 
 
-results = colorCalculation(video.frame,...
-    video.time.SWindex,...
-    video.colorScale.colorScaleRGBdouble,...
-    video.colorScale.colorScaleValue,...
-    video.coef,...
-    Shape,...
-    video.time.Bmode);
+%% set a shape (in the shape is the pixels selected to perform the color
+% comparaison)
+% Shape = setCustumShape(video(videoIndex).frame{frameIndex});             % custum
+Shape = setSquareShape(video(videoIndex).frame{frameIndex});               % as a square 
 
+%% compare each pixel contains in the shape (for each SWE image) to the
+results = colorCalculation(video(videoIndex).frame,...                     % frames 
+    video(videoIndex).time.SWindex,...                                     % index of the swe images
+    video(videoIndex).colorScale.colorScaleRGBdouble,...                   % colors of the scale 
+    video(videoIndex).colorScale.colorScaleValue,...                       % values of the scale 
+    video(videoIndex).coef,...                                             % scale value of the video dimension
+    Shape,...                                                              % shape
+    video(videoIndex).time.Bmode);                                         % time vecteur of the b mode
+
+%% plots 
+% time evolution 
 figureShearTemporalEvolution(results)
 
-figureFrameColorHistogram(video,1,1,results)
+% interestion value of an image
+sweFrameIndex = 6;
+figureFrameColorHistogram(video,videoIndex,sweFrameIndex,results)
 
-videoRead(video.frame,video.videoObject)
+%% save 
+currentForder = cd;                                                        % current forlder 
+cd(video(videoIndex).videoObject.Path)                                     % save in the video folder 
+saveName = [video(videoIndex).name(1:end-4),'.mat'];                       % save name you can cahnge it 
+save(saveName,"results",'-mat')                                            % save
+cd(currentForder)                                                          % go backto the function folder 
+disp(['results : ' , saveName,' is save in ', video(videoIndex).videoObject.Path])
