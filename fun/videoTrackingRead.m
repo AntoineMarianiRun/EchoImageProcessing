96e32b0b1837c6ced8af1be99780f30e1512cc0d
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2024-10-14 16:20:52 +0400

    function to read the tracking results

--- fun/videoTrackingRead.m
+++ fun/videoTrackingRead.m
@@ -1,4 +1,4 @@
-function videoTrackingRead(frames, video_object,mkrTracked)
+function videoTrackingRead(frames, video_object,mkrTracked,funTracked)
 % video_read Play video frames in a figure window.
 %   video_read(frames, video_object) plays the video frames stored in the
 %   cell array 'frames' using the properties of 'video_object'.
@@ -7,10 +7,20 @@ function videoTrackingRead(frames, video_object,mkrTracked)
 %   - frames: Cell array containing video frames.
 %   - video_object: VideoReader object with video properties.
 %   - mkrTracked: VideoReader object with video properties.
-nMkr = size(mkrTracked,2);
+
+if nargin <3
+    nMkr = 0;
+    nFun = 0 ; 
+elseif nargin<4
+    nMkr = size(mkrTracked,2);
+    nFun = 0 ; 
+else 
+    nMkr = size(mkrTracked,2);
+    nFun = size(funTracked,2) ; 
+end
 
 % Create a figure window for video playback
-fig = figure('Name', 'Video Playback', 'Color', [1 1 1], ...
+fig = figure('Name', 'Video Playback', 'Color', [1 1 1],...
     'ToolBar', 'none', 'MenuBar', 'none');
 
 % Calculate frame rate
@@ -33,6 +43,14 @@ for current_frame = 1 : 2 : video_object.NumFrames
             "MarkerSize",mkrTracked(mkr).markerSize)
     end 
     
+    for fun = 1 : nFun
+        plot( [funTracked(fun).limInf(current_frame), funTracked(fun).limSup(current_frame)],...            ...
+            [funTracked(fun).fun(current_frame).frame(funTracked(fun).limInf(current_frame)), ...
+            funTracked(fun).fun(current_frame).frame(funTracked(fun).limSup(current_frame))],...
+            "Color",funTracked(fun).color, ...
+            "LineStyle",funTracked(fun).lineStyle, ...
+            "LineWidth",funTracked(fun).lineWidth)
+    end
 
     % Calculate wait time to maintain frame rate
     elapsedTime = toc;
