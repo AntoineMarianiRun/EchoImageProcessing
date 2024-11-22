736ab458aa9e467664ffff83e7926a87b31331f7
AntoineMarianiRun <antoine.mariani@univ-reunion.fr>
2024-11-22 15:20:22 +0400

    change in prescale (multi select ok )

--- fun/preScaleVideo.m
+++ fun/preScaleVideo.m
@@ -3,7 +3,7 @@ function preScale = preScaleVideo()
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [f_,pathname] = uigetfile({'*.avi','Video (*.avi)';...
     '*.*'  ,'Tous les Fichiers (*.*)';},...
-    'Select your video','MultiSelect', 'off');
+    'Select your video','MultiSelect', 'on');
 f = {};% le ou les fichiers
 
 if iscell(f_)
@@ -18,85 +18,107 @@ if ischar(f_)
     f_ = string(f_);
 end
 
-disp(['Pre scaling of : ',  f_{1}])
-%% about Bmode
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% get frame and video information
-[frames, video_object] = loadVideo(f{1});
-
-% time of video
-timeBmode = 1/video_object.FrameRate : 1/video_object.FrameRate : video_object.Duration;
-if length(timeBmode) ~= video_object.NumFrames
-    timeBmode = linspace(0,video_object.Duration,size(frames,2));
-end
-%% scale process
-stateScaling = false;
-
-while stateScaling == false
-    % scale image
-    disp('     - Scale image ')
-
-    [coef_x,coef_y] = scaleImage(frames);% video scaling
-
-    % add tu the structure preScale
-    preScale.name = f_{1};
-    preScale.path = f{1};
-    preScale.time.Bmode = timeBmode;
-    preScale.coef.x = coef_x; % col factor
-    preScale.coef.y = coef_y; % row factor
-
-    %% Grey mapping
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     opts.Interpreter = 'tex'; opts.Default = 'Yes'; %option
-%     dlg = questdlg('Do you want to perform grey level analysis?','echo type',"Yes","No",opts);
-%     if strcmpi(string(dlg),"Yes")
-%         disp('     - Select zone to perform grey level analysis ')
-%         greyShape = setSquareShape(frames{1});
-%         preScale.greyShape = greyShape;
-%     end
 
 
-    %% color map
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    opts.Interpreter = 'tex'; opts.Default = 'Yes'; %option
-    dlg = questdlg('Do you want to perform shear extraction?','echo type',"Yes","No",opts);
-    if strcmpi(string(dlg),"Yes")
-        % about color scale
-        disp('     - Set color scale ')
-        frameRead(frames,1)
-        [colorScaleRGBuint8,colorScaleRGBdouble,colorScaleValue] = setColorScale(frames{1});
-        close all
+%% pre scaling
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nbFiles = length(f_);                                                      % number of file selected
+if ~isnumeric(f_)
+    for currentFile = 1 : nbFiles
 
-%         % about Zone of interest
-%         disp('     - Select zone to perform color analysis ')
-%         dlg1 = questdlg('what type of zone you want?','echo type',"Auto","Square","Custum",opts);
-%         if strcmpi(string(dlg1),"Auto")
-%             sweShape = autoShape(frames{1},preScale.coef);
-%         elseif strcmpi(string(dlg1),"Square")
-%             sweShape = setSquareShape(frames{1});
-%         elseif strcmpi(string(dlg1),"Custum")
-%             sweShape = setCustumShape(frames{1});
-%         end
+        % display to user
+        %%%%%%%%%%%%%%%%%
+        disp(['Scaling process of video : ', num2str(currentFile), ' / ', ...
+            num2str(nbFiles)])
+        disp(['video name : ', f_{currentFile}])
 
-        disp('     - Set SWE frame Rate ')
-        [timeSWE,indexImgSWE]= setSSIFrameRate(frames,timeBmode);
-        preScale.time.SWE = timeSWE;
-        preScale.time.SWindex = indexImgSWE;
+        % import video
+        %%%%%%%%%%%%%%
+        [frames, video_object] = loadVideo(f{currentFile});                    % import frames and video information
+        timeBmode = 1/video_object.FrameRate : ...
+            1/video_object.FrameRate :...
+            video_object.Duration;                                             % time vetor of the video
 
-        preScale.colorScale.colorScaleRGBuint8 = colorScaleRGBuint8;
-        preScale.colorScale.colorScaleRGBdouble = colorScaleRGBdouble;
-        preScale.colorScale.colorScaleValue = colorScaleValue;
-%         preScale.sweShape = sweShape;
-
-        dlg = questdlg('is prescaling ok?','echo type',"Yes","No","Abort",opts);
-        if strcmpi(string(dlg),"Yes")
-            stateScaling = true;
-            saveName = [preScale.name(1:end-4),'.mat'];
-            save(saveName,"preScale")
-        elseif strcmpi(string(dlg),"Abort")
-            stateScaling = true;
+        if length(timeBmode) ~= video_object.NumFrames
+            timeBmode = linspace(0,video_object.Duration,size(frames,2));      % correct potential error
         end
 
-    end
+        % scaling
+        %%%%%%%%%%%%%%
+        stateScaling = false;
+        while stateScaling == false
+            preScale = [];
 
+            % B-mode
+            %%%%%%%%%%%%%%
+            disp('     - Set scale the width and heigth of video ')
+            [coef_x,coef_y] = scaleImage(frames);                          % video scaling
+
+            % add to the structure preScale
+            preScale.name = f_{currentFile};
+            preScale.path = f{currentFile};
+            preScale.time.Bmode = timeBmode;
+            preScale.coef.x = coef_x; % col factor
+            preScale.coef.y = coef_y; % row factor
+
+            try
+                % Swe
+                %%%%%%%%%%%%%%
+                % color scale
+                disp('     - Set color scale ')
+                frameRead(frames,1)
+                [colorScaleRGBuint8,colorScaleRGBdouble,colorScaleValue] = setColorScale(frames{1}); % color scale
+                close all
+
+                % add to the structure preScale
+                preScale.colorScale.colorScaleRGBuint8 = colorScaleRGBuint8;
+                preScale.colorScale.colorScaleRGBdouble = colorScaleRGBdouble;
+                preScale.colorScale.colorScaleValue = colorScaleValue;
+
+                % frame rate
+                disp('     - Set SWE frame rate ')
+                [timeSWE,indexImgSWE]= setSSIFrameRate(frames,timeBmode);
+
+                % add to the structure preScale
+                preScale.time.SWE = timeSWE;
+                preScale.time.SWindex = indexImgSWE;
+            catch
+                disp('this file did not contain SWE map')
+                close all
+            end
+
+            % OK
+            %%%%%%%%%%%%%%
+            opts.Interpreter = 'tex'; opts.Default = 'Yes'; % options
+            dlg = questdlg('is prescaling ok?','echo type',"Yes","No","Abort",opts);
+            if strcmpi(string(dlg),"Yes")
+                stateScaling = true;
+                savePrescale(preScale)
+            elseif strcmpi(string(dlg),"Abort")
+                stateScaling = true;
+            end
+        end
+    end
 end
+end
+
+
+function savePrescale(preScale)
+origin = cd;
+
+saveName = [preScale.name(1:end-4),'.mat'];
+
+% find the path 
+savePath = preScale.path;
+tf = savePath == '/'| savePath == '\';
+idx = find(tf,1,"last");
+savePath =savePath(1 : idx-1);
+
+cd(savePath)
+save(saveName,"preScale",'-mat')
+cd(origin)
+
+disp(['file : ', saveName, ' save in : ',savePath ])
+
+
+end 
