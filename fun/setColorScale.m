4ec315087d81b56bd2e9e68e80add289ece32204
Antoine Mariani <antoine.mariani@univ-reunion.fr>
2025-04-24 16:26:39 +0400

    color scale not found

--- fun/setColorScale.m
+++ fun/setColorScale.m
@@ -1,5 +1,11 @@
 function [colorScaleRGBuint8,colorScaleRGBdouble,colorScaleValue] = setColorScale(img)
+try
     [colorScaleRGBuint8,colorScaleRGBdouble] = getColorScale(img);     % get the scale color
+catch Me
+    disp('Color scale not found, default coloscale set on')
+    [colorScaleRGBuint8,colorScaleRGBdouble] = getDefaultColorScale();     % get the scale color
+
+end
     [colorScaleValue] = setColorscaleValue(colorScaleRGBuint8,...
         colorScaleRGBdouble);                                          % get the corresponding value
 end
