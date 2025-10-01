95c2a01a272f9fab652eb60c84a33745aa3d323a
Antoine Mariani <antoine.mariani@univ-reunion.fr>
2025-10-01 19:05:55 +0400

    cancel usless variables

--- fun/colorCalculation.m
+++ fun/colorCalculation.m
@@ -9,34 +9,41 @@ pause(.5)
 
 % initialisation
 %%%%%%%%%%%%%%%%%%%
-n = size(frame,2); % Taille des vecteurs
+n = size(frame,2); % nombre d'image
 
-results.pixValue = cell(1, n);
-results.time = timeBmode;
-results.frameIndex = frameindex;
-results.meanValueYoungModulus = nan(1, n);
-results.stdValueYoungModulus = nan(1, n);
-results.meanValueShearVelocity = nan(1, n);
-results.stdValueShearVelocity = nan(1, n);
-results.meanValue = nan(1, n);
-results.stdValue = nan(1, n);
+% about zone of interest
 results.voidPercent = nan(1, n);
 results.saturationPercent = nan(1, n);
 results.nbPixel = nan(1, n);
 results.ROIsize = nan(1, n);
 
+% about value
+results.pixValue = cell(1, n);
+results.time = timeBmode;
+results.frameIndex = frameindex;
+results.meanValueYoungModulus = nan(1, n);
+results.stdValueYoungModulus = nan(1, n);
+results.meanValueShearModulus = nan(1, n);
+results.stdValueShearModulus = nan(1, n);
+results.meanValueShearVelocity = nan(1, n);
+results.stdValueShearVelocity = nan(1, n);
+
 
 for currentFrame = frameindex
     [resultsFrame] = frameColorCalculation(frame{currentFrame},colorScaleRGBdouble,colorScaleValue,coef,Shape);
-    results.pixValue{currentFrame} = resultsFrame.pixValue;
-    results.meanValueShearModulus(currentFrame) = resultsFrame.meanValueShearModulus;
-    results.stdValueShearModulus(currentFrame) = resultsFrame.stdValueShearModulus;
+
+    % about zone of interest
     results.voidPercent(currentFrame) = resultsFrame.voidPercent;
     results.saturationPercent(currentFrame) = resultsFrame.saturationPercent;
     results.nbPixel(currentFrame) = resultsFrame.nbPixel;
     results.ROIsize(currentFrame) = resultsFrame.ROIsize;
+
+    % about value
+    results.pixValue{currentFrame} = resultsFrame.pixValue;
     results.meanValueYoungModulus(currentFrame) = resultsFrame.meanValueYoungModulus;
     results.stdValueYoungModulus(currentFrame) = resultsFrame.stdValueYoungModulus;
+    results.meanValueShearModulus(currentFrame) = resultsFrame.meanValueShearModulus;
+    results.stdValueShearModulus(currentFrame) = resultsFrame.stdValueShearModulus;
     results.meanValueShearVelocity(currentFrame) = resultsFrame.meanValueShearVelocity;
     results.stdValueShearVelocity(currentFrame) = resultsFrame.stdValueShearVelocity;
 
