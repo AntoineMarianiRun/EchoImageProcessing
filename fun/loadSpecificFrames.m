9977e6b9885214e2e5f412919a548dca0dfe2477
Antoine Mariani <antoine.mariani@univ-reunion.fr>
2025-10-10 08:58:23 +0400

    importe specific frames

---
+++ fun/loadSpecificFrames.m
@@ -0,0 +1,87 @@
+function [frames, video_object] = loadSpecificFrames(videoPath,frameNumber,fig)
+% load_video Load video frames from a specified path.
+%   [frames, video_object] = load_video(path) loads the video specified by
+%   the given path and returns the frames in a cell array and the VideoReader
+%   object.
+%   [frames, video_object] = load_video(path, fig) uses the specified figure
+%   handle for displaying the progress dialog.
+%
+%   Input:
+%   - path: String or character vector specifying the path to the video file.
+%   - fig (optional): Figure handle for displaying the progress dialog.
+%
+%   Output:
+%   - frames: Cell array containing video frames.
+%   - video_object: VideoReader object with video properties.
+
+% Input verification
+if ischar(videoPath)
+    videoPath = string(videoPath);
+elseif isstring(videoPath)
+    if numel(videoPath) ~= 1
+        error('Input must be a single path.');
+    end
+else
+    error('Path must be a string or character vector.');
+end
+
+
+% Create VideoReader object
+videObject = VideoReader(videoPath);
+video_object.Name = videObject.Name;
+video_object.Path = videObject.Path;
+video_object.Duration = videObject.Duration;
+video_object.Width = videObject.Width;
+video_object.Height = videObject.Height;
+video_object.FrameRate = videObject.FrameRate;
+video_object.BitsPerPixel = videObject.BitsPerPixel;
+video_object.VideoFormat = videObject.VideoFormat;
+
+
+
+% % Handle optional figure input
+if nargin == 2
+    fig = uifigure('Name', 'Video Import Tool', 'Color', [1 1 1]);
+    fig.Position(3:4) = [400 100];  % Width and height
+    fig.WindowStyle = 'modal';      % Modal mode
+elseif nargin == 1
+    frameNumber = 1 : 1 :  round(video_object.Duration*video_object.FrameRate);
+end
+
+video_object.NumFrames = length(frameNumber);
+
+
+
+% Progress dialog
+if ~isempty(fig)
+    d = uiprogressdlg(fig, 'Title', 'Please Wait', 'Message', ['Loading video : ', video_object.Name]);
+end
+
+% Number of frames
+frames = cell(1,length(frameNumber));  % Initialize the cell array for storing frames
+
+
+for current_frame = frameNumber
+    try
+        frames{current_frame} = read(videObject, current_frame);
+        if ~isempty(fig)
+            d.Value = current_frame / video_object.NumFrames;  % Update progress bar
+        end
+    catch
+        video_object.NumFrames = current_frame-1 ;
+    end
+end
+
+
+
+% Update progress dialog message
+% d.Message = 'Finishing';
+pause(0.3);
+
+% Close the figure if it was created within the function
+if nargin == 2
+    if ~isempty(fig)
+        close(fig);
+    end
+end
+end
